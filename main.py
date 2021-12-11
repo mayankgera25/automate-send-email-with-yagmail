b7170b86a1385c8c1ab8305a35052e9b7086efd0
MayankGera <>
2021-12-11 18:17:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import yagmail
+import os
+
+sender = 'abc1@gmail.com'
+receiver = 'abc2@gmail.com'
+
+subject = "This is the subject"
+
+contents ="""
+Here is the content of the email!
+Hi!
+"""
+sender_password = input(f'Please enter the password for {sender}: \n')
+yag = yagmail.SMTP(user=sender, password=sender_password)
+
+yag.send (to=receiver, subject = subject, contents=contents)
+
+
+print('EMail Sent')
